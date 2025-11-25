5daef825ecc6b74d7153d256c034ce54edc50b85
GQ <gaoqiaoshiwo@mail.ustc.edu.cn>
2025-11-25 08:02:27 +0800

    recover jastrow

--- vmcnet/gaoqiao/networks.py
+++ vmcnet/gaoqiao/networks.py
@@ -2312,9 +2312,10 @@ def fermi_net_orbitals_part2(
     orbitals = [jnp.concatenate(orbitals, axis=1)] #[(ndet,nele,nele)]
   #本质上，ndet=1时，对于输入的orbitals_in:(nele,64)->[(n_up,64),(n_dn,64)]->[(n_up,nele),(n_dn,nele)]->[(nele,nele)] 
   #等效于：第一维是nele个轨道，第二维是nele个电子填充。
-  if params['jastrow'] is not None:
-    jastrow = options.jastrow.apply(params['jastrow'], r_ee, model_h_to_orbitals)
-    orbitals = [orbital * jastrow for orbital in orbitals ]
+  
+  # if params['jastrow'] is not None:
+  #   jastrow = options.jastrow.apply(params['jastrow'], r_ee, model_h_to_orbitals)
+  #   orbitals = [orbital * jastrow for orbital in orbitals ]
 
   return orbitals, (ae, r_ae, r_ee, model_h_to_orbitals, hz)
 
@@ -2369,6 +2370,10 @@ def fermi_net(
   if return_det_dist:
     return jax.nn.log_softmax(logdet)
   
+  if params['jastrow'] is not None:
+    jastrow = options.jastrow.apply(params['jastrow'], r_ee, he)
+    log_out += jastrow
+
   return sign_out, log_out
 
 def orbitals_fn(
